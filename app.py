7a348a78f432ef76305cd28bf9c6f2e130faffed
Pavel Zaiats <Pavel_Zaiats8503@jabil.com>
2021-04-29 00:03:04 +0300

    Course

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
-# Product Class/Model
+# Course Class/Model
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=True)
